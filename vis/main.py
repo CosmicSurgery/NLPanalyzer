d1a44df1c254d98d681f27bbe4ea9e59b944fbb0
CosmicSurgery <mileskeating@gmail.com>
2025-05-01 19:47:21 -0700

    The dashboard loads the df from ("data.h5") in main.py and passes it on to the tab

--- vis/main.py
+++ vis/main.py
@@ -2,10 +2,17 @@ from bokeh.io import curdoc
 from bokeh.themes import built_in_themes
 from bokeh.models.layouts import Tabs
 
+import pandas as pd
+import os
+
 from tab1 import tab1
 
 
-tab1 = tab1()
+
+PATH = os.getcwd()
+df = pd.read_hdf(os.path.join(PATH,"vis","demo.h5"))
+
+tab1 = tab1(df)
 
 tabs = Tabs(tabs = [tab1])
 
